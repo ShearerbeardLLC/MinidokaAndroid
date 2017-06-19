8c01dd19bee6205c05c5ad428ad049418e93f94e
Shearerbeard <shearerbeard@gmail.com>
2017-06-19 00:30:01 -0600

    formatting fix

--- components/Map.js
+++ components/Map.js
@@ -1,18 +1,69 @@
-import React, { Component } from "react";
+import React, {
+	Component
+} from "react";
 import MapView from 'react-native-maps';
 import {
-  Text,
-  View
+	Text,
+	View
 } from 'react-native';
+import {
+	StyleSheet
+} from 'react-native';
+import {
+	DEFAULT_LOCATION
+} from "../const/map";
+import sitesData from "../const/sites.json";
 
-import styles from "../styles/Container";
+const styles = StyleSheet.create({
+	container: {
+		...StyleSheet.absoluteFillObject,
+		height: 400,
+		width: 400,
+		justifyContent: 'flex-end',
+		alignItems: 'center',
+	},
+	map: {
+		...StyleSheet.absoluteFillObject,
+	},
+});
 
 export default class Map extends Component {
-  render() {
-    return (
-      <View style={ styles.container }>
-        <Text style={ styles.subContainer }>Map</Text>
-      </View>
-    );
-  }
+	constructor(...args) {
+		super(...args);
+
+		this.state = {
+			region: DEFAULT_LOCATION
+		};
+
+		this.onRegionChange = this.onRegionChange.bind(this);
+	}
+
+	onRegionChange(region) {
+		this.setState({
+			region
+		});
+	}
+
+	render() {
+		return (
+			<View style={ styles.container }>
+				<MapView
+					style={ styles.map }
+					region={ this.state.region }
+					onRegionChange={ this.onRegionChange }>
+				{ sitesData
+					.map(({prefix, name, detail, coordinates}) =>
+						<MapView.Marker
+							key={ prefix }
+							title={ name }
+							description={ detail }
+							coordinate={{
+								latitude: coordinates[0].latitude,
+								longitude: coordinates[0].longitude
+						}} />)
+					}
+				</MapView>
+			</View>
+		);
+	}
 }
--- const/map.js
+++ const/map.js
@@ -1,11 +1,10 @@
 
 const DEFAULT_LOCATION = {
 	latitude: 42.68067,
-	longitude: -114.24607
+	longitude: -114.24607,
 	latitudeDelta: 0.0922,
 	longitudeDelta: 0.0421
 };
-
 export {
 	DEFAULT_LOCATION
 };
