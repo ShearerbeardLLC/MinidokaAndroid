0c1ba3a725f7472625857d7b62f3f0428e8fdb4a
Shearerbeard <shearerbeard@gmail.com>
2017-07-30 19:48:52 -0600

    moves site photos to their own component

--- components/Site.js
+++ components/Site.js
@@ -1,19 +1,13 @@
 import React, { Component } from "react";
 import {
   StyleSheet,
-  Text,
   View,
-  Image,
   Dimensions
 } from "react-native";
 
 import styles from "../styles/Container";
+import SitePhotos from './SitePhotos';
 
-import { siteToPhotos } from "../util/site";
-
-import Carousel from 'react-native-looped-carousel';
-
-const { width } = Dimensions.get('window');
 const viewStyles = StyleSheet.create({
   column: {
     flexDirection: 'column',
@@ -29,50 +23,11 @@ const siteContainerStyles = StyleSheet.flatten([
 
 export default class Site extends Component {
 
-  constructor(...args) {
-    super(...args);
-
-    this.state = {
-      size: this.setSize({width})
-    }
-  }
-
-  _onLayoutDidChange = (e) => {
-    const layout = e.nativeEvent.layout;
-    this.setSize(layout);
-  }
-
-  _onPageChange(i) {
-  }
-
-  setSize({width}) {
-    return {
-      width,
-      height: width * 0.75
-    };
-  }
-
-  renderImages() {
-    const photos = siteToPhotos(this.props);
-    return photos.map(({previewUrl}, i) =>
-      <Image style={this.state.size} key={i} source={ previewUrl } />)
-  }
-
   render() {
 		const { name, subDetail, photos, text } = this.props;
     return (
-      <View
-        style={ siteContainerStyles }
-        onLayout={ this._onLayoutDidChange }>
-        <Carousel
-          delay={2000}
-          style={this.state.size}
-          autoplay
-          pageInfo
-          onAnimateNextPage={ this._onPageChange }
-        >
-        { this.renderImages() }
-        </Carousel>
+      <View style={ siteContainerStyles } >
+        <SitePhotos {...this.props} />
       </View>
     );
   }
---
+++ components/SitePhotos.js
@@ -0,0 +1,58 @@
+
+import React, { Component } from "react";
+import {
+  Image,
+  Dimensions
+} from "react-native";
+import Carousel from 'react-native-looped-carousel';
+
+import { siteToPhotos } from "../util/site";
+
+const { width } = Dimensions.get('window');
+
+export default class SitePhotos extends Component {
+
+  constructor(...args) {
+    super(...args);
+
+    this.state ={
+      size: this.setSize({width})
+    };
+  }
+
+  setSize({width}) {
+    return {
+      width,
+      height: width * 0.75
+    };
+  }
+
+  _onLayoutDidChange = (e) => {
+    const layout = e.nativeEvent.layout;
+    this.setSize(layout);
+  }
+
+  _onPageChange(i) {
+  }
+
+  renderImages() {
+    const photos = siteToPhotos(this.props);
+    return photos.map(({previewUrl}, i) =>
+      <Image style={this.state.size} key={i} source={ previewUrl } />)
+  }
+
+  render() {
+    return (
+      <Carousel
+        delay={2000}
+        style={this.state.size}
+        autoplay
+        pageInfo
+        onLayout={ this._onLayoutDidChange }
+        onAnimateNextPage={ this._onPageChange }
+      >
+        { this.renderImages() }
+      </Carousel>
+    );
+  }
+}
