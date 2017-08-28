647fff0b20e448ed56ee66a148c801ed01cf5b85
Shearerbeard <shearerbeard@gmail.com>
2017-08-27 22:42:14 -0600

    fixes gas station

--- components/Grant.js
+++ components/Grant.js
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
     }),
     marginLeft: 8,
     marginRight: 8,
-    marginBottom: 16
+    marginBottom: 58
   },
   text: {
     color: 'black',
@@ -29,18 +29,22 @@ const styles = StyleSheet.create({
   }
 });
 
+const txt =
+`This project was funded, in part, by a grant from the U.S. Department of the Interior, National Park Service, Japanese American Confinement Sites Grant Program. Any opinions, findings, and conclusions or recommendations expressed in this material are those of the author(s) and do not necessarily reflect the views of the U.S. Department of the Interior.
+
+This material received Federal financial assistance for the preservation and interpretation of U.S. confinement sites where Japanese Americans were detained during World War II. Under Title VI of the Civil Rights Act of 1964, Section 504 of the Rehabilitation Act of 1973, and the Age Discrimination Act of 1975, as amended, the U.S. Department of the Interior prohibits discrimination on the basis of race, color, national origin, disability or age in its federally funded assisted projects. If you believe you have been discriminated against in any program, activity, or facility as described above, or if you desire further information, please write to:
+
+Office of Equal Opportunity
+National Park Service
+1849 C Street, NW
+Washington, DC 20240
+`;
+
 const Grant = () => (
   <View style={ styles.container }>
     <ScrollView>
       <Text style={ styles.text }>
-      This project was funded, in part, by a grant from the U.S. Department of the Interior, National Park Service, Japanese American Confinement Sites Grant Program. Any opinions, findings, and conclusions or recommendations expressed in this material are those of the author(s) and do not necessarily reflect the views of the U.S. Department of the Interior. 
-
-This material received Federal financial assistance for the preservation and interpretation of U.S. confinement sites where Japanese Americans were detained during World War II. Under Title VI of the Civil Rights Act of 1964, Section 504 of the Rehabilitation Act of 1973, and the Age Discrimination Act of 1975, as amended, the U.S. Department of the Interior prohibits discrimination on the basis of race, color, national origin, disability or age in its federally funded assisted projects. If you believe you have been discriminated against in any program, activity, or facility as described above, or if you desire further information, please write to: 
-
-Office of Equal Opportunity 
-National Park Service 
-1849 C Street, NW 
-Washington, DC 20240
+        { txt }
       </Text>
     </ScrollView>
   </View>
